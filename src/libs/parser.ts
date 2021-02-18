09a64d414acddd954b4972910406094ba78275b3
Huskiez <huskiez@pop-os.localdomain>
2021-02-18 21:54:23 +1000

    added grammar.js to gitignore

--- src/libs/parser.ts
+++ src/libs/parser.ts
@@ -15,6 +15,7 @@ export interface WarbandInfo {
 
 export function parseChatLine(message: string): Partial<WarbandInfo>|null {
     try {
+        
         return parse(message);
     } catch(e) {
         return null;
