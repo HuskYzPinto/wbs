a613a03f32bba557572fd1cee25fb12554803901
Huskiez <huskiez@pop-os.localdomain>
2021-02-24 17:26:42 +1000

    set up gh pages

---
+++ src/components/wbform.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import { LabeledStatement } from 'typescript';
+import logo from './logo.svg';
+
+const wbForm = () => {
+    let header = 'hello';
+
+}
+export default wbForm;
---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import wbForm from '../components/wbform';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        test
+      </p>
+    </div>
+  );
+}
+
+export default App;
