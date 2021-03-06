c251beec7dc47346f98c201bcd9d5531657299dc
Huskiez <huskiez@pop-os.localdomain>
2021-03-07 00:55:46 +1000

    applied prettier, table can be sorted asc or desc

--- src/components/WbTable.tsx
+++ src/components/WbTable.tsx
@@ -2,76 +2,80 @@ import React, { useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { done } from '../store/actions';
 import { WarbandInfo } from '../libs/parser';
-import {Camp} from '../store/type';
-
+import { Camp } from '../store/type';
 
 interface Props {}
 
-
 const WbTable: React.FC<Props> = () => {
-	
-    const state = useSelector((state) => state.camps);
+	const state = useSelector((state) => state.camps);
 	const dispatch = useDispatch();
 
-	const camps:Array<Camp> = state;
-	const [sortedField, setSortedField] = React.useState<any|null>(null);
-	
-	let sortedCamps:any = [...camps];
+	const camps: Array<Camp> = state;
+	const [sortConfig, setSortConfig] = React.useState<any>(null);
 
-	
+	const requestSort = (key: string) => {
+		let direction = 'ascending';
+		if (sortConfig) {
+			if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+				direction = 'descending';
+			}
+		}
+		setSortConfig({ key, direction });
+	};
 
-	if (sortedField !== null){
-		sortedCamps.sort((a:any,b:any) => {
-		if (a[sortedField!] < b[sortedField!]){
-			return -1;
-		}
-		if (a[sortedField!] > b[sortedField!]){
-			return 1;
-		}
-		return 0
+	let sortedCamps: any = [...camps];
+
+	if (sortConfig !== null) {
+		sortedCamps.sort((a: any, b: any) => {
+			if (a[sortConfig.key!] < b[sortConfig.key!]) {
+				return sortConfig.direction === 'ascending' ? -1 : 1;
+			}
+			if (a[sortConfig.key!] > b[sortConfig.key!]) {
+				return sortConfig.direction === 'ascending' ? 1 : -1;
+			}
+			return 0;
 		});
 	}
-	
 
-	
-	const checkBool = (info: boolean|null)=>{
-		if (info){
+	const checkBool = (info: boolean | null) => {
+		if (info) {
 			return 'Yes';
-		} else{
+		} else {
 			return 'No';
 		}
-	}
+	};
 
-	const checkTent = (tents: Array<string>|null, skill: string)=>{
-		if (tents){
+	const checkTent = (tents: Array<string> | null, skill: string) => {
+		if (tents) {
 			return tents.includes(skill);
 		} else {
-			return null
+			return null;
 		}
-	}
+	};
 
-
-    const output = sortedCamps.map((camp:any) => 
-        <tr key={camp.world}>
-            <td>{camp.world}</td>
-            <td>{camp.location}</td>
+	const output = sortedCamps.map((camp: any) => (
+		<tr key={camp.world}>
+			<td>{camp.world}</td>
+			<td>{camp.location}</td>
 			<td>{checkBool(checkTent(camp.tents, 'C'))}</td>
 			<td>{checkBool(checkTent(camp.tents, 'S'))}</td>
 			<td>{checkBool(checkTent(camp.tents, 'M'))}</td>
 			<td>{checkBool(checkTent(camp.tents, 'H'))}</td>
 			<td>{checkBool(checkTent(camp.tents, 'F'))}</td>
-            <td>{camp.state}</td>
-            <td>{checkBool(camp.pker)}</td>
+			<td>{camp.state}</td>
+			<td>{checkBool(camp.pker)}</td>
 			<td>{checkBool(camp.done)}</td>
 			<td>
-				<button onClick={() => {
-					const info: Partial<WarbandInfo> = {world:camp.world};
-					dispatch(done(info))}}>
-					Done </button>
+				<button
+					onClick={() => {
+						const info: Partial<WarbandInfo> = { world: camp.world };
+						dispatch(done(info));
+					}}>
+					Done{' '}
+				</button>
 			</td>
-
-        </tr>
-    );
+		</tr>
+	));
 	return (
 		<div>
 			<table>
@@ -79,40 +83,58 @@ const WbTable: React.FC<Props> = () => {
 				<thead>
 					<tr>
 						<th>
-							<button type="button" onClick={() =>setSortedField('world')}>World</button>
+							<button type="button" onClick={() => requestSort('world')}>
+								World
+							</button>
 						</th>
 						<th>
-							<button type="button" onClick={() => setSortedField('location')}>Location</button>
+							<button type="button" onClick={() => requestSort('location')}>
+								Location
+							</button>
 						</th>
 						<th>
-							<button type="button" onClick={() => setSortedField('construction')}>Construction</button>
+							<button type="button" onClick={() => requestSort('construction')}>
+								Construction
+							</button>
 						</th>
 						<th>
-							<button type="button" onClick={() => setSortedField('smithing')}>Smithing</button>
+							<button type="button" onClick={() => requestSort('smithing')}>
+								Smithing
+							</button>
 						</th>
 						<th>
-							<button type="button" onClick={() => setSortedField('mining')}>Mining</button>
+							<button type="button" onClick={() => requestSort('mining')}>
+								Mining
+							</button>
 						</th>
 						<th>
-							<button type="button" onClick={() => setSortedField('herblore')}>Herblore</button>
+							<button type="button" onClick={() => requestSort('herblore')}>
+								Herblore
+							</button>
 						</th>
 						<th>
-							<button type="button" onClick={() => setSortedField('farming')}>Farming</button>
+							<button type="button" onClick={() => requestSort('farming')}>
+								Farming
+							</button>
 						</th>
 						<th>
-							<button type="button" onClick={() => setSortedField('status')}>Status</button>
+							<button type="button" onClick={() => requestSort('status')}>
+								Status
+							</button>
 						</th>
 						<th>
-							<button type="button" onClick={() => setSortedField('pker')}>Pker</button>
+							<button type="button" onClick={() => requestSort('pker')}>
+								Pker
+							</button>
 						</th>
 						<th>
-							<button type="button" onClick={() => setSortedField('done')}>Done</button>
+							<button type="button" onClick={() => requestSort('done')}>
+								Done
+							</button>
 						</th>
 					</tr>
 				</thead>
-				<tbody>
-                    {output}
-				</tbody>
+				<tbody>{output}</tbody>
 			</table>
 		</div>
 	);
